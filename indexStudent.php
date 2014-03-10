5737343f0e1f2c82c77e10baea8af2f5feec13ac
Fedde <feddevangils@gmail.com>
2014-03-10 15:03:39 +0100

    indexStudent.php toegevoegd

---
+++ indexStudent.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Film lijst</title>
+    <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+
+      <!-- Acteurs tabel -->
+      <div class="row">
+        <div class="panel panel-default">
+        <div class="panel-body">
+            <h1>Geboortedata van acteurs <span class="glyphicon glyphicon-user"></span> </h1>
+            <table class="table table-striped">
+               <thead>
+               <tr>
+                   <th>Voornaam</th>
+                   <th>Achternaam</th>
+                   <th>Geboortedatum</th>
+               </tr>
+               </thead>
+               <tbody>
+               
+               </tbody>
+            </table>
+        </div>
+        </div>
+      </div>
+
+      <!-- Genres tabel -->
+      <div class="row">
+         <div class="panel panel-default">
+        <div class="panel-body">
+            <h1>Alle genres uit de film database <span class="glyphicon glyphicon-tag"></span> </h1>
+            <table class="table table-striped">
+               <thead>
+               <tr>
+                   <th>Genre</th>
+                   <th>Beschrijving</th>
+               </tr>
+               </thead>
+               <tbody>
+               
+               </tbody>
+            </table>
+        </div>
+        </div>
+      </div>
+
+      <!-- Films tabel -->
+      <div class="row">
+         <div class="panel panel-default">
+        <div class="panel-body">
+            <h1>Alle films uit de Film database <span class="glyphicon glyphicon-facetime-video"></span> </h1>
+            <table class="table table-striped">
+               <thead>
+               <tr>
+                   <th>titel</th>
+                   <th>jaar</th>
+               </tr>
+               </thead>
+               <tbody>
+               
+               </tbody>
+            </table>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+</body>
+</html>
